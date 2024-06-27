93073d44084314434c882a2c19c83c882e706980
VikasSivashankaran <vikasbmca@gmail.com>
2024-06-27 19:34:20 +0530

    file

--- List/Maximum and min element in a List.py
+++ List/Maximum and min element in a List.py
@@ -1,4 +1,11 @@
-def Minmax(arr):
-    arr.sort
-    minmax={"min":arr[0],"max":arr[1]}
+
+def getMinMax(arr):
+    arr.sort()
+    minmax = {"min": arr[0], "max": arr[-1]}
     return minmax
+
+arr = [1,85,96,456,78,258,45]
+minmax = getMinMax(arr)
+
+print("Minimum element is", minmax["min"])
+print("Maximum element is", minmax["max"])
