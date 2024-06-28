4d66cf801e29971a3920c6be79ecdb128fea2a7a
VikasSivashankaran <vikasbmca@gmail.com>
2024-06-28 15:19:27 +0530

    vf

--- List/ways to clear list.py
+++ List/ways to clear list.py
@@ -2,3 +2,19 @@ list=[859,"apple",75545,"orange",'grapes',85]
 print("list:",list)
 list.clear()
 print("after clear:",list)
+
+l1=[4,5,6]
+print(l1)
+while (len(l1)!=0):
+    l1.pop()
+    print(l1)
+
+l2=[7,8,94,2,4,5]
+print(l2)
+del l2[:]
+print(l2)
+
+l3=[78,45,12,6,5,4]
+print(l3)
+l3=[]
+print(l3)
