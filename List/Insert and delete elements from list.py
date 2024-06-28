a93cc79b7458956f7d447553da59fbbc765a3169
VikasSivashankaran <vikasbmca@gmail.com>
2024-06-28 20:12:41 +0530

    adding

---
+++ List/Insert and delete elements from list.py
@@ -0,0 +1,10 @@
+list=['a','g','o','f']
+list.append('b')
+print(list)
+
+list.pop(2)
+print(list)
+
+listt=[1,2,3,5]
+listt.remove(3)
+print(listt)
---
+++ Tuples/Sum of tuple elements.py
@@ -0,0 +1,5 @@
+tuple1=(1,5,6)
+print(tuple1)
+
+tu=sum(tuple1)
+print(tu)
