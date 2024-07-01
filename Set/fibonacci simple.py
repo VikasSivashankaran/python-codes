f0e7b388adfef2ed6e94475c86ce49313cc56a5e
VikasSivashankaran <vikasbmca@gmail.com>
2024-07-01 10:54:55 +0530

    new

--- Set/fibonacci simple.py
+++ Set/fibonacci simple.py
@@ -1,4 +1,4 @@
-n=10
+'''n=10
 n1=0
 n2=1
 next=n2
@@ -9,4 +9,19 @@ while count<=n:
     count+=1
     n1,n2=n2,next
     next=n1+n2
-    print()
+    print()'''
+'''
+def fibonacci(n):
+    if n<0:
+        print("invalid")
+    elif n==0:
+        return 0
+    elif n==1 or n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
+print(fibonacci(10))
+   '''
+     
+    
+    
