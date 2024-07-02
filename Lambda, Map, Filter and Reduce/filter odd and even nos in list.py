88c42ea5f8e124c5f3ad90e9a4d60bcb240584ed
VikasSivashankaran <vikasbmca@gmail.com>
2024-07-02 15:57:04 +0530

    new

--- Lambda, Map, Filter and Reduce/filter odd and even nos in list.py
+++ Lambda, Map, Filter and Reduce/filter odd and even nos in list.py
@@ -2,3 +2,6 @@ lists=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print("Original List",lists)
 dict=list(filter(lambda x: x % 2 ==0,lists))
 print("Even numbers from list:",dict)
+
+dict1=list(filter(lambda x: x % 2 != 0,lists))
+print("odd number :",dict1)
