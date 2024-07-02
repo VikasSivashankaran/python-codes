609985cfc47365fd6a02d05e4585d3c18fd85e98
VikasSivashankaran <vikasbmca@gmail.com>
2024-07-02 16:09:25 +0530

    new

---
+++ Lambda, Map, Filter and Reduce/given string starts with a given character using Lambda.py
@@ -0,0 +1,9 @@
+start_with= lambda x: True if x.startswith('p') else False
+
+
+print(start_with('python'))
+print(start_with('Java'))
+
+str=lambda x: True if x.startswith('R') else False
+print(str('Rohit'))
+print(str('ravi')) 
